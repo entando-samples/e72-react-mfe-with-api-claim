7bb300c6e01695db6e54aaed3cf60ccf222117fa
Nathan Shaw <n.shaw@entando.com>
2023-06-28 12:47:44 -0400

    Init from e72 react with config

---
+++ microfrontends/simple-mfe/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App({config}) {
+  const { params } = config || {};
+  const { name } = params || {};
+
+  return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Hello, {name}!
+          </p>
+        </header>
+      </div>
+  );
+}
+
+export default App;
